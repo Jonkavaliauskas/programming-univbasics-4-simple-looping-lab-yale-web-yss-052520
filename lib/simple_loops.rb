ecf8bd898eefa71472c69099fcbf8c82b7bc9068
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-18 07:58:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,4 +17,13 @@ def loop_message_n_times(string, n)
     counter += 1
   end
 end
-  
+
+
+def output_array(array)
+  counter = 0
+ 
+  while counter < n do
+    puts string
+    counter += 1
+  end
+end  
